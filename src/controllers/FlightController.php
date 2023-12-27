9d3c104b827ab281a6a90078281058c1f8a1189e
greatyassoo <greatyassoo@yahoo.com>
2023-12-28 00:19:42 +0200

    update FlightController for new schema and add Search function

--- src/controllers/FlightController.php
+++ src/controllers/FlightController.php
@@ -9,8 +9,13 @@ require_once "../airplane-booking-php/config/database.php";
     fee DECIMAL(10, 2) NOT NULL,
     passenger_capacity INT NOT NULL,
     company_id INT NOT NULL,
-    FOREIGN KEY (company_id) REFERENCES Company(id)
-    );
+    start_city_id INT NOT NULL,
+    end_city_id INT NOT NULL,
+    FOREIGN KEY (company_id) REFERENCES Company(id),
+    FOREIGN KEY (start_city_id) REFERENCES City(id),
+    FOREIGN KEY (end_city_id) REFERENCES City(id)
+);
+
 */
 class FlightController{
 
@@ -81,25 +86,58 @@ class FlightController{
 
     // add Flight
     /**
-     * Returns an array of Flights from the 'Flight' table in the database, where the keys are the column names and the values are the corresponding values in the database.
-     * The Flights are filtered by the provided company ID.
-     * If no Flights with the provided company ID are found in the database, an empty array is returned.
+     * Inserts a new flight into the 'Flight' table in the database.
+     * The new flight is associated with a specific company, identified by the provided company ID, and has a start and end city, identified by their respective IDs.
      *
-     * @param int $company_id The ID of the company to filter Flights by.
-     * @return array An array of associative arrays representing the Flights with the provided company ID.
+     * @param string $name The name of the flight.
+     * @param float $fee The fee for the flight.
+     * @param int $passenger_capacity The passenger capacity of the flight.
+     * @param int $company_id The ID of the company to associate the flight with.
+     * @param int $start_city_id The ID of the city where the flight starts.
+     * @param int $end_city_id The ID of the city where the flight ends.
+     * @return bool Returns true if the flight was successfully inserted, false otherwise.
      */
-    public static function addFlight($name, $fee, $passenger_capacity, $company_id){
+    public static function addFlight($name, $fee, $passenger_capacity, $company_id, $start_city_id, $end_city_id){
         $db = Database::getInstance();
         $conn = $db->getConnection();
 
-        $sql = "insert into Flight (name, fee, passenger_capacity, company_id) 
-                values ('$name', $fee, $passenger_capacity, $company_id)";
-    
+        $sql = "insert into Flight(name, fee, passenger_capacity, company_id, start_city_id, end_city_id)
+                values ('$name', $fee, $passenger_capacity, $company_id, $start_city_id, $end_city_id)";
+
         if ($conn->query($sql) === TRUE) {
             return true;
         } else {
             return false;
         }
     }
+
+    // search Flights start city and end city
+    /**
+     * Searches for flights in the 'Flight' table in the database that start and end in the specified cities.
+     * The cities are identified by their respective IDs.
+     * Returns an array of flights that match the criteria, where each flight is represented as an associative array with keys as column names and values as the corresponding values in the database.
+     * If no flights match the criteria, an empty array is returned.
+     *
+     * @param int $start_city_id The ID of the city where the flight starts.
+     * @param int $end_city_id The ID of the city where the flight ends.
+     * @return array An array of associative arrays representing the flights that match the criteria.
+     */
+    public static function searchFlights($start_city_id, $end_city_id){
+        $db = Database::getInstance();
+        $conn = $db->getConnection();
+
+        $sql = "select * from Flight where start_city_id = $start_city_id and end_city_id = $end_city_id";
+        $result = $conn->query($sql);
+
+        $Flights = array();
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                array_push($Flights, $row);
+            }
+        }
+        return $Flights;
+    }
+
+
 }
 ?>
